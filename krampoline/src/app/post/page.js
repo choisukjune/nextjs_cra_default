262e8b33a1808527a5df6e3a6e9a454161b43b84
sukjune.choi <sukjune.choi@gmail.com>
2023-09-19 10:54:34 +0900

    dddd

--- krampoline/src/app/post/page.js
+++ krampoline/src/app/post/page.js
@@ -21,7 +21,7 @@ async function getData() {
 
 export default async function Home() {
   
-  const reqData = await getData();
+  const data = await getData();
     
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -51,7 +51,7 @@ export default async function Home() {
             POST
       </h2>
       <h2>
-      {reqData}
+      {data}
       </h2>
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing 
