339b63aa039357cf8fe607d4ac053481755ec498
sukjune.choi <sukjune.choi@gmail.com>
2023-09-19 11:35:00 +0900

    testst

--- krampoline/src/app/post/page.js
+++ krampoline/src/app/post/page.js
@@ -23,8 +23,6 @@ async function ReaTest() {
 
 export default async function Home() {
   
-  const data = await getData();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
