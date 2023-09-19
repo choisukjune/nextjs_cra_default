fba804104fbd32b68c6d6063193bf3b56179a019
sukjune.choi <sukjune.choi@gmail.com>
2023-09-19 11:08:11 +0900

    test

--- krampoline/src/app/post/page.js
+++ krampoline/src/app/post/page.js
@@ -78,7 +78,7 @@ export default async function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              ->
+              -&gt
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -95,7 +95,7 @@ export default async function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Learn{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              ->
+              -&gt
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -112,7 +112,7 @@ export default async function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              ->
+              -&gt
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -129,7 +129,7 @@ export default async function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              ->
+              -&gt
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
