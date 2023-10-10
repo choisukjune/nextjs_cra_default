0e9f8b68135c76cb1446990c63cf31af0af5670c
sukjune.choi <sukjune.choi@gmail.com>
2023-10-10 17:07:52 +0900

    test

--- krampoline/src/app/about/page.js
+++ krampoline/src/app/about/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Nav from '../components/Nav';
 
 import Image from 'next/image'
-const basePath = process.env.BASE_PATH || "";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -19,7 +19,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src={basePath + "/vercel.svg" }
+              src={"/vercel.svg" }
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -33,13 +33,13 @@ export default function Home() {
             ABOUT
       </h2>
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          Get started by editing 
           <code className="font-mono font-bold">src/app/page.js</code>
       </p>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src={basePath + "/next.svg" }
+          src={"/next.svg" }
           alt="Next.js Logo"
           width={180}
           height={37}
@@ -57,7 +57,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              ->
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -74,11 +74,11 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Learn{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              ->
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about Next.js in an interactive course with quizzes!
           </p>
         </a>
 
@@ -91,7 +91,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              ->
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -108,7 +108,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              ->
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
